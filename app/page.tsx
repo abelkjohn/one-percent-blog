24852a5cb8917cb6555ce71c7d8af9adbb6b3006
abelkjohn <126896178+abelkjohn@users.noreply.github.com>
2023-11-22 18:01:17 +0530

    modified:   app/page.tsx 	new file:   components/Header.jsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
-import Image from 'next/image'
-
+import React from 'react'
+import Header from '../components/Header'
 export default function Home() {
   return (
-    <h1>One Percent</h1>
+    <Header />
   )
 }
+
---
+++ components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header(){
+ return (
+    <div className='flex flex-col items-end w-8/12 mx-auto mt-20'>
+      <p>&quot;Embrace the power of incremental growth—where every 1 percent improvement each day becomes the foundation for a stronger, more resilient you tomorrow.&quot;</p>
+      <p>-ChatGPT</p>
+    </div>
+ )
+}
