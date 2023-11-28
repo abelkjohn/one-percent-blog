73727eb1ad204de341667ecae6b7806b384a91cd
abelkjohn <126896178+abelkjohn@users.noreply.github.com>
2023-11-28 23:50:48 +0530

    modified:   components/BlogLists.js

--- components/BlogLists.js
+++ components/BlogLists.js
@@ -18,5 +18,14 @@ const getPosts = async () => {
 export default async function PostList(){
  const { blog } = await getPosts()
 
-    return JSON.stringify(blog.map(i => i.title))
+    return blog.map(i => {
+        return <div className='flex '>
+            <h1>{i.title}</h1>
+            <p>{i.description}</p>
+            <p>{i.createdAt.split('').splice(0, 10).join('')}</p>
+            <p>{i.skills.map(i => i + ",")}</p>
+            <p>{i.day}</p>
+        </div>
+    })
+
 }
