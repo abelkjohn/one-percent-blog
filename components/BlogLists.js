9f3504167fde3d19798f7453f4c3787b865b447f
abelkjohn <126896178+abelkjohn@users.noreply.github.com>
2023-12-02 22:33:54 +0530

    modified:   components/BlogLists.js

--- components/BlogLists.js
+++ components/BlogLists.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const getPosts = async () => {
     try {
-        const res = await fetch('https://one-percent-blog.vercel.app/api/posts', {
+        const res = await fetch('http://localhost:3000/api/posts', {
             cache: "no-store"
         })
         if (!res.ok){
